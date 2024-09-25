f06fbfe283c8a4f4f60f26268be95a414eb03ef9
FUNKTASTHIC <ignacioavendanoramirez@gmail.com>
2024-09-25 00:01:14 -0300

    feat: add validator middleware

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,15 +5,19 @@ const { login } = require('../controllers/authController');
 const { validateToken } = require('../controllers/authController');
 const { register } = require('../controllers/authController');
 
+// Validators
+const { loginValidator } = require('../validators/authValidator');
+const { registerValidator } = require('../validators/authValidator');
+
 const router = Router();
 
+// Register user
+router.post('/register', [registerValidator], register);
+
 // Login user
-router.post('/login', login);
+router.post('/login', [loginValidator], login);
 
 // Verify token
 router.get('/verify-token', validateToken);
-
-// Register
-router.post('/register', register);
 
 module.exports = router;
--- src/routes/searchRoutes.js
+++ src/routes/searchRoutes.js
@@ -8,15 +8,28 @@ const { getStudentGradesAndRestrictions } = require('../controllers/searchContro
 const { getStudentByRestrictionOrReason } = require('../controllers/searchController');
 const { getStudentsByGradeRange } = require('../controllers/searchController');
 
+// Validators
+const { getStudentGradesAndRestrictionsValidator } = require('../validators/searchValidator');
+const { getStudentByRestrictionOrReasonValidator } = require('../validators/searchValidator');
+const { getStudentsByGradeRangeValidator } = require('../validators/searchValidator');
+
 const router = Router();
 
 // Search student grades and restrictions
-router.get('/student', [validateJWT], getStudentGradesAndRestrictions);
+router.get(
+  '/student',
+  [validateJWT, getStudentGradesAndRestrictionsValidator],
+  getStudentGradesAndRestrictions
+);
 
 // Search student by restriction or reason
-router.get('/restriction', [validateJWT], getStudentByRestrictionOrReason);
+router.get(
+  '/restriction',
+  [validateJWT, getStudentByRestrictionOrReasonValidator],
+  getStudentByRestrictionOrReason
+);
 
 // Search student by grade range
-router.get('/grade', [validateJWT], getStudentsByGradeRange);
+router.get('/grade', [validateJWT, getStudentsByGradeRangeValidator], getStudentsByGradeRange);
 
 module.exports = router;
